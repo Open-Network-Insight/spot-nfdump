9acf3b5e7c585a4c36727870aa4438b79a70ea5c
natedogs911 <natedogs911@gmail.com>
2017-02-08 11:13:36 -0800

    switch to default log_cmd

--- install_nfdump.sh
+++ install_nfdump.sh
@@ -22,10 +22,10 @@ fi
 
 if [ -f /etc/redhat-release ]; then
     install_cmd="yum -y install"
-    echo "installation command: $install_cmd"
+    log_cmd "installation command: $install_cmd"
 elif [ -f /etc/debian_version ]; then
     install_cmd="apt-get install -y"
-    echo "installation command: $install_cmd"
+    log_cmd "installation command: $install_cmd"
     apt-get update
 fi
 
@@ -36,9 +36,9 @@ fi
 
 for dep in ${dependencies[@]}; do
     if type ${dep} >/dev/null 2>&1; then
-        echo "${dep} found"
+        log_cmd "${dep} found"
     else
-        echo "installing ${dep}"
+        log_cmd "installing ${dep}"
         ${install_cmd} ${dep}
     fi
 done
@@ -53,13 +53,13 @@ make
 make install
 
 if type nfdump >/dev/null 2>&1; then
-    echo "nfdump found"
+    log_cmd "nfdump found"
 else
-    echo "ERROR: there was a problem with the nfdump installation"
+    log_cmd "ERROR: there was a problem with the nfdump installation"
     exit 1      
 fi
 
 cd ..
 
-echo "Done !!!!!!!!!!!!!!!!!!!!!!!!"
+log_cmd "Done !!!!!!!!!!!!!!!!!!!!!!!!"
 nfdump -V
